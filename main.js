5c66ef07ddaa659aa72d6c7431048f823f0857c2
antonipanti <133729226+antonipanti@users.noreply.github.com>
2023-12-13 00:11:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let cancion = document.getElementById("cancion");
+let play_btn = document.getElementById("play-btn");
+let pause_btn = document.getElementById("pause-btn");
+let stop_btn = document.getElementById("stop-btn");
+let contenedor = document.getElementById("contenedor");
+let gatito = document.getElementById("gatito");
+
+play_btn.addEventListener("click",()=>{
+    cancion.play();
+  
+    gatito.style.animationPlayState ="running";
+
+})
+pause_btn.addEventListener("click", ()=>{
+    cancion.pause();
+    contenedor.style.animationPlayState ="paused";
+    gatito.style.animationPlayState ="paused";
+
+   
+})
+stop_btn.addEventListener("click", ()=>{
+    cancion.pause();
+    cancion.currentTime =0;
+    contenedor.style.animationPlayState ="paused";
+    gatito.style.animationPlayState ="paused";
+})
